cb3f82129ece4128571a74ce58dc1296dacb38d4
Danny Milosavljevic <dannym@friendly-machines.com>
2025-02-09 21:37:36 +0100

    early-init: Make toolbar (and menu) a lot more useful.

--- early-init.el
+++ early-init.el
@@ -1,4 +1,5 @@
 
+
 ;; TODO: Report bug with trashed upstream that this is missing.
 (define-key global-map
   [menu-bar tools trashed]
@@ -24,6 +25,7 @@
   [menu-bar tools wttrin]
   '("Check weather" . wttrin))
 
+;; TODO: Report bug with emacs upstream that this is missing.
 (define-key global-map
   [menu-bar tools maxima]
   '("Do computer algebra via Maxima" . maxima))
@@ -31,41 +33,60 @@
 (defun tool-bar-setup ()
   (setq tool-bar-separator-image-expression
     (tool-bar--image-expression "separator"))
-  (tool-bar-add-item-from-menu 'find-file "new" nil :label "New File"
-                   :vert-only t)
-  (tool-bar-add-item-from-menu 'menu-find-file-existing "open" nil
-                   :label "Open" :vert-only t)
-  ; too big (tool-bar-add-item-from-menu 'dired "diropen" nil :vert-only t)
-  (tool-bar-add-item-from-menu 'kill-this-buffer "close" nil :vert-only t)
-  (tool-bar-add-item-from-menu 'save-buffer "save" nil
-                   :label "Save")
-  (define-key-after (default-value 'tool-bar-map) [separator-1] menu-bar-separator)
-  (tool-bar-add-item-from-menu 'undo "undo" nil)
-  (define-key-after (default-value 'tool-bar-map) [separator-2] menu-bar-separator)
-  (tool-bar-add-item-from-menu (lookup-key menu-bar-edit-menu [cut])
-                   "cut" nil :vert-only t)
-  (tool-bar-add-item-from-menu (lookup-key menu-bar-edit-menu [copy])
-                   "copy" nil :vert-only t)
-  (tool-bar-add-item-from-menu (lookup-key menu-bar-edit-menu [paste])
-                   "paste" nil :vert-only t)
-  (define-key-after (default-value 'tool-bar-map) [separator-3] menu-bar-separator)
+  (tool-bar-add-item "search"
+                       #'swiper-isearch
+                       'swiper-isearch :label "swiper-isearch"
+                       :help "Swiper-isearch...")
+
+  ;(tool-bar-add-item-from-menu 'isearch-forward "search"
+  ;                 nil :label "Search" :vert-only t)
+  ;;(tool-bar-add-item-from-menu 'ispell-buffer "spell")
+
+  (let ((tool-bar-map (default-value 'tool-bar-map)))
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/ripgrep")
+                       #'consult-ripgrep
+                       'consult-ripgrep :label "consult-ripgrep"
+                       :help "Consult ripgrep...")
+
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/embark-act")
+                       #'embark-act
+                       'embark-act :label "embark-act"
+                       :help "Embark act")
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/magit")
+                       #'magit
+                       'magit :label "magit"
+                       :help "Magit (git)")
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-store-link")
+                       #'org-store-link
+                       'org-store-link :label "org-store-link"
+                       :help "Store Org link")
+    (define-key-after tool-bar-map [separator-2] menu-bar-separator)
+    ; Subsumed by org-node-find for my workflow!
+    ;(tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-capture")
+    ;                   #'org-capture
+    ;                   'org-capture :label "org-capture"
+    ;                   :help "Capture Org node...")
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-capture")
+                       #'org-node-find
+                       'org-node-find :label "org-node-find"
+                       :help "Find or make Org node...")
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-node-grep")
+                       #'org-node-grep
+                       'org-node-grep :label "org-node-grep"
+                       :help "Grep Org node...")
+    (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-agenda")
+                       #'org-agenda
+                       'org-agenda :label "org-agenda"
+                       :help "Show Org agenda...")))
+
+;; Global version.
+(defun qtool-bar-setup ()
+  (setq tool-bar-separator-image-expression
+    (tool-bar--image-expression "separator"))
   (tool-bar-add-item-from-menu 'swiper-isearch "search"
                    nil :label "Search" :vert-only t)
   ;;(tool-bar-add-item-from-menu 'ispell-buffer "spell")
 
-  ;; There's no icon appropriate for News and we need a command rather
-  ;; than a lambda for Read Mail.
-  ;;(tool-bar-add-item-from-menu 'compose-mail "mail/compose")
-
-  ;; Help button on a tool bar is rather non-standard...
-  ;; (let ((tool-bar-map (default-value 'tool-bar-map)))
-  ;;   (tool-bar-add-item "help" (lambda ()
-  ;;                (interactive)
-  ;;                (popup-menu menu-bar-help-menu))
-  ;;               'help
-  ;;               :help "Pop up the Help menu"))
-
-  (let ((tool-bar-map (default-value 'tool-bar-map)))
     (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/ripgrep")
                        #'consult-ripgrep
                        'consult-ripgrep :label ""
@@ -79,12 +100,6 @@
                        #'magit
                        'magit :label ""
                        :help "Magit (git)")
-                       )
-
-  (define-key-after (default-value 'tool-bar-map) [separator-4] menu-bar-separator)
-  
-  ;;; org
-  (let ((tool-bar-map (default-value 'tool-bar-map)))
     (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-store-link")
                        #'org-store-link
                        'org-store-link :label ""
@@ -105,9 +120,7 @@
     (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/org-agenda")
                        #'org-agenda
                        'org-agenda :label ""
-                       :help "Show Org agenda..."))
-  (define-key-after (default-value 'tool-bar-map) [separator-5] menu-bar-separator)
-  (let ((tool-bar-map (default-value 'tool-bar-map)))
+                       :help "Show Org agenda...")
     (tool-bar-add-item (expand-file-name "~/.emacs.d/icons/mu4e")
                        #'mu4e
                        'mu4e :label ""
@@ -116,14 +129,4 @@
                        #'elfeed
                        'elfeed :label ""
                        :help "Elfeed (RSS)")
-    (tool-bar-add-item-from-menu 'gptel
-                                 (expand-file-name "~/.emacs.d/icons/gptel")
-                                 nil :label "Talk to LLM"
-                                 :vert-only t)
-    (tool-bar-add-item-from-menu 'osm
-                                 (expand-file-name "~/.emacs.d/icons/osm")
-                                 nil :label "View street map" :vert-only t)
-    (tool-bar-add-item-from-menu 'emms
-                                 (expand-file-name "~/.emacs.d/icons/emms")
-                                 nil :label "Play music..."
-                                 :vert-only t)))
+)
