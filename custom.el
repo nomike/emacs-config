ee54d5150ad15bd2b73a3ca8de22a9841bd79dd3
Danny Milosavljevic <dannym@friendly-machines.com>
2025-03-28 23:31:19 +0100

    Setup gnus some more.

--- custom.el
+++ custom.el
@@ -2332,6 +2332,29 @@ This function is called by `org-babel-execute-src-block'."
    (dot . t)
    (gnuplot . t)
    (plantuml . t)))
+;; TODO: Switch to notmuch for news as well?
+(use-package gnus
+  :config
+  (when window-system
+    (setq gnus-sum-thread-tree-indent "  ")
+    (setq gnus-sum-thread-tree-root "") ;; "● "
+    (setq gnus-sum-thread-tree-false-root "") ;; "◯ "
+    (setq gnus-sum-thread-tree-single-indent "") ;; "◎ "
+    (setq gnus-sum-thread-tree-vertical        "│")
+    (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+    (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
+  (setq gnus-summary-line-format
+        (concat
+         "%0{%U%R%z%}"
+         "%3{│%}" "%1{%d%}" "%3{│%}" ;; date
+         "  "
+         "%4{%-20,20f%}"               ;; name
+         "  "
+         "%3{│%}"
+         " "
+         "%1{%B%}"
+         "%s\n"))
+  (setq gnus-summary-display-arrow t)
                                         ; gnus-dired-attach in dired; attaches to open email
   (setq gnus-play-startup-jingle nil)
                                         ; Also .newsrc
