e2615e1e4846c65c243ea76046994ce060f65623
Danny Milosavljevic <dannym@friendly-machines.com>
2025-04-05 21:55:30 +0200

    prog-mode: Add direct staging shortcuts.

--- custom.el
+++ custom.el
@@ -2569,7 +2569,8 @@ This function is called by `org-babel-execute-src-block'."
   (setq xenops-math-image-scale-factor 0.75))
 (add-hook 'org-mode-hook #'my/org-scale-up-font)
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
-(with-eval-after-load 'magit
+
+(with-eval-after-load 'prog-mode
   (define-key prog-mode-map (kbd "C-c s") 'magit-stage-region)
   (define-key prog-mode-map (kbd "C-c c") 'magit-commit))
 
