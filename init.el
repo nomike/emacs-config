eaae768f1509e5ad12aa15bb2083cd5a724f38eb
Danny Milosavljevic <dannym@friendly-machines.com>
2025-05-12 23:53:11 +0200

    Switch to lualatex.

--- init.el
+++ init.el
@@ -94,6 +94,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-command "lualatex -shell-escape")
+ '(LaTeX-electric-left-right-brace t)
+ '(TeX-engine 'luatex)
  '(back-button-no-wrap t)
  '(buffer-env-safe-files
    '(("/home/dannym/src/guix/manifest.scm" . "0b387290e9851813debd81b6e3aa5099f0f17fad1fade821ca1f0928262e56c4")
