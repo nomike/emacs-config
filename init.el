78264327a4f8421481b7dfa0a099650afb874fbc
nomike <nomike@nomike.com>
2025-05-28 01:30:29 +0200

    Remove rogue "~" character

--- init.el
+++ init.el
@@ -834,11 +834,10 @@
 
 (recentf-mode 1)
 (setq recentf-max-menu-items 25)
-~
+
 (require 'exec-path-from-shell)
 (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE" "NIX_SSL_CERT_FILE" "NIX_PATH" "GUIX_TEXMF" "GUIX_LOCPATH"))
   (add-to-list 'exec-path-from-shell-variables var))
 
 (when (memq window-system '(mac ns x pgtk))
   (exec-path-from-shell-initialize))
- 
