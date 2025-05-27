f2acbf5421e73b9953fbbdd026118d0bb8598d4b
nomike <nomike@nomike.com>
2025-05-28 01:48:50 +0200

    Revert "Change key-binding C-w to kill the current buffer instead of burrying it"
    This reverts commit b9a40b5b78b0541374d155bc368a32fa1cd7d023.

--- custom.el
+++ custom.el
@@ -1678,7 +1678,6 @@ argument is given. Choose a file name based on any document
 
 (keymap-set global-map "M-3" 'delete-window)
 (keymap-set global-map "M-#" `delete-other-windows)
-(keymap-set wakib-keys-overriding-map "C-w" `kill-current-buffer)
 
 (keymap-set global-map "C-<f5>" `eval-region)
 
